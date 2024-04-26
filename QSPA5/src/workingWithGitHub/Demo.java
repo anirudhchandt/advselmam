bbbe1081a14026a4211ff560de17a5dd3dda5b1b
Apoorva <Apoorva@TYSS-Apoorva>
2024-04-26 12:33:16 +0530

    Just update.

--- QSPA5/src/workingWithGitHub/Demo.java
+++ QSPA5/src/workingWithGitHub/Demo.java
@@ -3,5 +3,6 @@ package workingWithGitHub;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello github");
+		System.out.println("hello done with the git hub");
 	}
 }
